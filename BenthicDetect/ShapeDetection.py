bd711f12515bc248f0d70922ec5bf3e54d03b276
Adam Graham <adam3.graham@gmail.com>
2019-02-02 10:04:41 -0800

    Initial Code Upload

---
+++ BenthicDetect/ShapeDetection.py
@@ -0,0 +1,53 @@
+# USAGE
+# python detect_shapes.py --image shapes_and_colors.png
+
+
+# import the necessary packages
+from pyimagesearch.shapedetector import ShapeDetector
+import argparse
+import imutils
+import cv2
+import numpy as np
+
+
+# load the image and resize it to a smaller factor so that
+
+image = cv2.imread("benthic.png")
+resized = imutils.resize(image, width=536)
+ratio = image.shape[0] / float(resized.shape[0])
+
+# convert the resized image to grayscale, blur it slightly,
+# and threshold it
+gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+lower = np.array([0, 0, 0])
+upper = np.array([60, 60, 60])
+thresh = cv2.inRange(image, lower, upper)
+# find contours in the thresholded image and initialize the
+# shape detector
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+sd = ShapeDetector()
+
+# loop over the contours
+for c in cnts:
+	# compute the center of the contour, then detect the name of the
+	# shape using only the contour
+	M = cv2.moments(c)
+	cX = int((M["m10"] / M["m00"]) * ratio)
+	cY = int((M["m01"] / M["m00"]) * ratio)
+	if M["m00"] == 0: shape = "line"
+	shape = sd.detect(c)
+
+	# multiply the contour (x, y)-coordinates by the resize ratio,
+	# then draw the contours and the name of the shape on the image
+	c = c.astype("float")
+	c *= ratio
+	c = c.astype("int")
+	cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+	cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+		0.5, (100, 100, 100), 2)
+	# show the output image
+cv2.imshow("Image", image)
+cv2.waitKey(0)
