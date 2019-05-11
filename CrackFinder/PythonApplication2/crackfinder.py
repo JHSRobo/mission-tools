7838744e85c5dcda012ed045119a0cf7adf58539
Shatteredpixelz <adam3.graham@gmail.com>
2019-05-10 20:59:35 -0700

    mike pls help with black square detecc

--- CrackFinder/PythonApplication2/crackfinder.py
+++ CrackFinder/PythonApplication2/crackfinder.py
@@ -20,7 +20,7 @@ while True:
 	ratio = frame.shape[0] / float(resized.shape[0])
 	hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 	#blurred = cv2.GaussianBlur(hsv, (5, 5), 0)
-	lower_red = np.array([103,140,50])
+	lower_red = np.array([100,100,50])
 	upper_red = np.array([130,255,255])
 	k = cv2.waitKey(33)
 	if k==97:
@@ -48,15 +48,22 @@ while True:
 			new_img = frame[y:y+h,x:x+w]
 			cv2.imshow('croppo',new_img)
 			cv2.imwrite('croppo.png', new_img)
+			M = cv2.moments(c)
+			cX = int((M["m10"] / (M["m00"] + 1e-7)) * ratio)
+			cY = int((M["m01"] / (M["m00"] + 1e-7)) * ratio)
+			c = c.astype("float")
+			c *= ratio
+			c = c.astype("int")
+			cv2.drawContours(frame, [c], -1, (0, 0, 255), 2)
 		if shape == "rectangle":
 			cv2.imwrite("is-it-a-rectange.png", mask)
 			sqtest = cv2.imread("is-it-a-rectange.png", 0)
-			y = 110
-			x = 120
-			h = 285
-			w = 380
-			crop = sqtest[y:y+h, x:x+w]
-			thresh = cv2.adaptiveThreshold(crop,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,2)
+			y = 55
+			x = 60
+			h = 485
+			w = 480
+			#crop = sqtest[y:y+h, x:x+w]
+			thresh = cv2.adaptiveThreshold(sqtest,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,13,2)
 			cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 			cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 			sd = ShapeDetector()
@@ -77,46 +84,58 @@ while True:
 					cv2.imwrite("foundthecrack.png", frame)
 					#cv2.imwrite("foundthecrack.png", frame)
 					image4 = frame
-					#gray3=cv2.cvtColor(image4,cv2.COLOR_BGR2GRAY)
-					#edged = cv2.Canny(image4, 10, 250)
-					lower = np.array([0, 0, 0])
-					upper = np.array([40, 40, 40])
-					thresh100 = cv2.inRange(image4, lower, upper)
-					#blurred3 = cv2.GaussianBlur(gray3, (5, 5), 0)
-					#thresh3 = cv2.threshold(blurred3, 60, 255, cv2.THRESH_BINARY)
-					cnts = cv2.findContours(thresh100.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+					gray3=cv2.cvtColor(image4,cv2.COLOR_BGR2GRAY)
+					#edged = cv2.Canny(image4, 170, 250)
+					#lower = np.array([0, 0, 0])
+					#upper = np.array([70, 70, 70])
+					#thresh100 = cv2.inRange(image4, lower, upper)
+					blurred3 = cv2.GaussianBlur(gray3, (5, 5), 0)
+					#thresh3 = cv2.threshold(blurred3, 100, 255, cv2.THRESH_BINARY)
+					thresh3 = cv2.adaptiveThreshold(blurred3 ,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,13,4)
+					#cnts = cv2.findContours(thresh100.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+					cnts = cv2.findContours(thresh3.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+					#cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 					cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-					cv2.imshow('blackcrop', thresh100)
+					#cv2.imshow('blackcrop', thresh100)
+					cv2.imshow('blackcrop', thresh3)
+					#cv2.imshow('blackcrop', edged)
 					for c in cnts:
 						x,y,w,h = cv2.boundingRect(c)
-						if w>200 and h>200:
+						if w>100 and h>100:
 							new_img1=image4[y:y+h,x:x+w]
 							cv2.imshow('blackcrop.png', new_img1)
 							cv2.imwrite('blackcrop.png', new_img1)
-					heightblk = np.size(new_img1, 0)
-					widthblk = np.size(new_img1, 1)
-					heightblu = np.size(new_img, 0)
-					widthblu = np.size(new_img, 1)
-					print("blkpixel height", heightblk)
-					print("blkpixel width", widthblk)
-					print("bluepixel width", widthblu)
-					print("bluepixel height", heightblu)
-					actualheightblk = 30.0/heightblk
-					actualwidthblk = 30.0/widthblk
-					actualheightblu = actualheightblk*heightblu
-					actualwidthblu = actualwidthblk*widthblu
-					print("actualheightblue", actualheightblu)
-					print("actualwidthblue", actualwidthblu)
+							heightblk = np.size(new_img1, 0)
+							widthblk = np.size(new_img1, 1)
+							heightblu = np.size(new_img, 0)
+							widthblu = np.size(new_img, 1)
+							print("blkpixel height", heightblk)
+							print("blkpixel width", widthblk)
+							print("bluepixel width", widthblu)
+							print("bluepixel height", heightblu)
+							actualheightblk = 31.0/heightblk
+							actualwidthblk = 31.0/widthblk
+							actualheightblu = actualheightblk*heightblu
+							actualwidthblu = actualwidthblk*widthblu
+							print("actualheightblue", actualheightblu)
+							print("actualwidthblue", actualwidthblu)
+							M = cv2.moments(c)
+							cX = int((M["m10"] / (M["m00"] + 1e-7)) * ratio)
+							cY = int((M["m01"] / (M["m00"] + 1e-7)) * ratio)
+							c = c.astype("float")
+							c *= ratio
+							c = c.astype("int")
+							cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
+							#cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100, 100, 100), 2)
+							#sys.exit("found the crack!")
+							k = 97
+							break
+						else:
+							try:
+								os.remove("foundthecrack.png")
+							except: pass
 
-					sys.exit("found the crack!")
-					k = 97
-					break
-				else:
-					try:
-						os.remove("foundthecrack.png")
-					except: pass
-
-			# multiply the contour (x, y)-coordinates by the resize ratio,
+			# multiply the contour (x, y)-coordinates by the resigratio
 			# then draw the contours and the name of the shape on the image
 			c = c.astype("float")
 			c *= ratio
