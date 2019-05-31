a8e1a291fb4fabdfa03568169df23dea96bcb9a3
Shatteredpixelz <adam3.graham@gmail.com>
2019-05-31 16:30:15 -0700

    crack detecc just sorta fixed but needs testing

--- crack_finder/src/crack_finder.py
+++ crack_finder/src/crack_finder.py
@@ -87,79 +87,36 @@ def find_the_shape():
 				cY = int((M["m01"] / (M["m00"] + 1e-7)) * ratio)
 				shape = sd.detect(c)
 				x,y,w,h = cv2.boundingRect(c)
-				if w > 4 and h > 4:
+				if w>1 and h>1:
 					idx+=1
 					new_img = frame[y:y+h,x:x+w]
 					cv2.imshow('croppo',new_img)
 					cv2.imwrite('croppo.png', new_img)
-				if shape == "rectangle":
-					cv2.imwrite("is-it-a-rectange.png", mask)
-					sqtest = cv2.imread("is-it-a-rectange.png", 0)
-					y = 110
-					x = 120
-					h = 285
-					w = 380
-					crop = sqtest[y:y+h, x:x+w]
-					thresh = cv2.adaptiveThreshold(crop,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,11,2)
-					cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-					cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-					sd = ShapeDetector()
-					for c in cnts:
-						# compute the center of the contour, then detect the name of the
-						# shape using only the contour
-						M = cv2.moments(c)
-						cX = int((M["m10"] / (M["m00"] + 1e-7)) * ratio)
-						cY = int((M["m01"] / (M["m00"] + 1e-7)) * ratio)
-						shape = sd.detect(c)
-						if shape == "rectangle":
-							c = c.astype("float")
-							c *= ratio
-							c = c.astype("int")
-							#cv2.drawContours(thresh, [c], -1, (0, 255, 0), 2)
-							#cv2.putText(thresh, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100, 100, 100), 2)
-							cv2.imshow('thresh',thresh)
-							#cv2.imwrite("foundthecrack.png", frame)
-							image4 = frame
-							#gray3=cv2.cvtColor(image4,cv2.COLOR_BGR2GRAY)
-							#edged = cv2.Canny(image4, 10, 250)
-							lower = np.array([0, 0, 0])
-							upper = np.array([40, 40, 40])
-							thresh100 = cv2.inRange(image4, lower, upper)
-							#blurred3 = cv2.GaussianBlur(gray3, (5, 5), 0)
-							#thresh3 = cv2.threshold(blurred3, 60, 255, cv2.THRESH_BINARY)
-							cnts = cv2.findContours(thresh100.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-							cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-							cv2.imshow('blackcrop', thresh100)
-							for c in cnts:
-								x,y,w,h = cv2.boundingRect(c)
-								if w > 200 and h > 200:
-									new_img1=image4[y:y+h,x:x+w]
-									cv2.imshow('blackcrop.png', new_img1)
-									cv2.imwrite('blackcrop.png', new_img1)
-									heightblk = np.size(new_img1, 0)
-									widthblk = np.size(new_img1, 1)
-									heightblu = np.size(new_img, 0)
-									widthblu = np.size(new_img, 1)
-									actualheightblk = 31.0/heightblk
-									actualwidthblk = 31.0/widthblk
-									actualheightblu = actualheightblk * heightblu
-									actualwidthblu = actualwidthblk * widthblu
-
-									msg.length = max(actualheightblu, actualwidthblu)
-									return msg
-					# multiply the contour (x, y)-coordinates by the resize ratio,
-					# then draw the contours and the name of the shape on the image
+					M = cv2.moments(c)
+					cX = int((M["m10"] / (M["m00"] + 1e-7)) * ratio)
+					cY = int((M["m01"] / (M["m00"] + 1e-7)) * ratio)
 					c = c.astype("float")
 					c *= ratio
 					c = c.astype("int")
-					#cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
-					#cv2.putText(frame, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (100, 100, 100), 2)
-
-				# multiply the contour (x, y)-coordinates by the resize ratio,
-				# then draw the contours and the name of the shape on the image
-				c = c.astype("float")
-				c *= ratio
-				c = c.astype("int")
+					cv2.drawContours(frame, [c], -1, (0, 0, 255), 2)
+					blueval0 = np.size(new_img, 0)
+					blueval1 = np.size(new_img, 1)
+					bluesmallside = min(blueval0, blueval1)
+					bluephatsize = max(blueval0, blueval1)
+					quickmaffsSMALLO = bluesmallside / 1.75
+					quickmaffsLARGO = bluesmallside / 1.87
+					lengthsmallo = bluephatsize / quickmaffsSMALLO
+					lengthLARGO = bluephatsize / quickmaffsLARGO
+					#print("if 1.8: ", lengthsmallo)
+					#print("if 1.9: ", lengthLARGO)
+					j = "if 1.8: "
+					i = str(lengthsmallo)
+					m = j + i
+					b = " if 1.9: "
+					o = str(lengthLARGO)
+					e = m + b + o
+					msg.length = e
+					return msg
 	msg.length = None
 	return msg
 
